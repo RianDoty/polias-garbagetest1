2822619415da742bfe2c796dc6cd8e4914ddde4b
Glitch (bustling-familiar-ceres) <none>
2021-10-05 01:56:19 +0000

    🎯💄 Checkpoint ./src/App.js:35575323/251

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import io from 'socket.io-client';
 import './App.css';
-const socket = io()
 
+const isDev = process.env.NODE_ENV === 'development';
 
+const socket = isDev 
+? io.connect('http://localhost:3001') 
+: io.connect();
 
 const { useState, useEffect } = React
 
