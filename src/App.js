950f0823d04beda2978fb40070aac9d06da5d2ae
Glitch (bustling-familiar-ceres) <none>
2021-10-07 19:21:49 +0000

    🐓🏁 Checkpoint ./src/App.js:35575323/177

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ const socket = io({path: '/api'})
 
 const { useState, useEffect } = React
 
-function useForceUpdate(){
-    const [value, setValue] = useState(0); // integer state
-    return () => setValue(value => value + 1); // update the state to force render
-}
-
 function App() {
   const [latest, setLatest] = useState()
   const [count, setCount] = useState(0)
