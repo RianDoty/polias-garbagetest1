03b0e0ea622fb1eee5bc5555a8c907ab33408f55
Glitch (bustling-familiar-ceres) <none>
2021-10-13 18:23:40 +0000

    🏰🌂 Checkpoint ./src/App.js:35575323/1637

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Router } from 'wouter';
-import PageRouter from './components/router';
+import React from "react";
+import logo from "./logo.svg";
+import { Router } from "wouter";
+import PageRouter from "./components/router";
 
-import './styles/App.css';
-import useSocket from './hooks/use-socket.js'
-import useHashLocation from './hooks/wouter-hash'
+import "./styles/App.css";
+import useSocket from "./hooks/use-socket.js";
+import useHashLocation from "./hooks/wouter-hash";
 
-
-const { useState, useEffect, useMemo } = React
-
+const { useState, useEffect, useMemo } = React;
 
 function App() {
-  const [latest, setLatest] = useState()
-  const [count, setCount] = useState(0)
-  
+  const [latest, setLatest] = useState();
+  const [count, setCount] = useState(0);
+
   useSocket({
-    'socket-connected': (id)=>{setCount(c=>c+1); setLatest(id)}
-    
-  })
-  
+    "socket-connected": id => {
+      setCount(c => c + 1);
+      setLatest(id);
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <main className='App-main'>
-        <PageRouter/>
-      </main>
+      <Router hook={useHashLocation}>
+        <header className="App-header"></header>
+        <main className="App-main">
+          <PageRouter />
+        </main>
+      </Router>
     </div>
   );
 }
