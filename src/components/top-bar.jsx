eca7435427444bef6a797ee32ea2d1912fd3c350
Glitch (bustling-familiar-ceres) <none>
2021-10-18 17:38:31 +0000

    🚆🌁 Checkpoint ./src/components/top-bar.jsx:35575323/206 ./src/styles/cooldude.css:35575323/187 ./src/styles/App.css:35575323/238 ./src/styles/header.css:35575323/218 ./src/styles/header-main.css:35575323/34

--- src/components/top-bar.jsx
+++ src/components/top-bar.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import '../styles/header.css';
+
+const Logo = () => {
+  return (
+    <div class='header-logo'>
+      <h1>Polias</h1>
+    </div>
+  )
+}
+
 export default function TopBar() {
   return (
     <header class='common-header'>
-      <div class='header-logo'>Polias</div>
+      <Logo/>
       <div class='header-content'></div>
       <div class='header-profile'></div>
     </header>
