9d2f1be211e7993726f371a50f07f2192417dd2f
Glitch (bustling-familiar-ceres) <none>
2021-10-20 18:26:39 +0000

    🐁🍝 Checkpoint ./src/hooks/profile.jsx:35575323/534

--- src/hooks/profile.jsx
+++ src/hooks/profile.jsx
@@ -1,2 +1,17 @@
 import {useState, useEffect} from 'react';
 
+const profile = {
+  name: 'unknown'
+}
+
+function useForceUpdate(){
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue(value => value + 1); // update the state to force render
+}
+
+const useProfile = () => {
+  const forceUpdate = useForceUpdate()
+  
+}
+
+export default useProfile;
