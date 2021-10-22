d25181bbce42f2e897b8217db342a5fd8ea5e7f5
Glitch (bustling-familiar-ceres) <none>
2021-10-22 07:48:14 +0000

    🙋🐏 Checkpoint ./src/styles/home.css:35575323/91 ./src/pages/home.js:35575323/400 ./src/styles/header.css:35575323/23

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,13 @@ export default function Home() {
   return (
     <div class='narrow'>
       <Section>
-        
+        <h1>Hello, and welcome!</h1>
+        <p>Polias is a deception game where <em>everyone</em> gets to have fun.</p>
+        <p>Scheme, steal, and make bank the fastest to buy the legendary idol and ascend to greatness!</p>
+      </Section>
+      <h2>Get Started</h2>
+      <Section>
+      
       </Section>
     </div>
   )
