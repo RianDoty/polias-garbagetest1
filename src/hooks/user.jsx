68e723822997888c2ca8d21677a0c053d9c81eee
Glitch (bustling-familiar-ceres) <none>
2021-10-22 20:58:37 +0000

    🌠👣 Checkpoint ./src/hooks/user.jsx:35575323/298

--- src/hooks/user.jsx
+++ src/hooks/user.jsx
@@ -1,24 +1,7 @@
 import { useState, useEffect } from 'react';
 
-class User {
-  constructor({
-    
-  }) {
-    
-  }
-  
-  refresh() {
-    return new User(this)
-  }
-}
-
-const useForceUpdate = () => {
-  const [index, setIndex] = useState(0)
-  return () => setIndex(i=>i+1)
-}
-
 const useUser = () => {
-  const [user, updateUser] = useState(new User())
+  const [name, setName] = useState('');
   
-  return user
+  return {name}
 }
