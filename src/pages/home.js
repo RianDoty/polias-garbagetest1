54582524dba97e47a1499c41dda91828e00110ae
Glitch (bustling-familiar-ceres) <none>
2021-10-22 07:37:10 +0000

    👡🛤 Checkpoint ./src/pages/home.js:35575323/258 ./src/styles/header.css:35575323/171 ./src/styles/App.css:35575323/54 ./src/styles/home.css:35575323/345

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import '../styles/home.css';
+
+const Section = ({children}) => (<div class='dash-section'>{children}</div>)
+
 export default function Home() {
   return (
-    <div>
-      Haha yes
+    <div class='narrow'>
+      <Section>
+        
+      </Section>
     </div>
   )
 }
