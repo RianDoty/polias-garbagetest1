64b20efb5ab25418d7ed81aa3f327d5109a7ecdd
Glitch (bustling-familiar-ceres) <none>
2021-10-24 23:47:33 +0000

    😅🚊 Checkpoint ./src/pages/game.jsx:35575323/109 ./src/components/router.jsx:35575323/108

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -3,11 +3,13 @@ import { Switch, Route } from 'wouter';
 
 //Page imports
 import Home from '../pages/home'
+import Game from '../pages/game'
 
 export default function PageRouter() {
   return (
     <Switch>
       <Route path='/' component={Home}/>
+      <Route path='/game/*' component={Game}/>
     </Switch>
   )
 }
