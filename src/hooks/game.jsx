15f21bdc8acdbf0d99ab5ce9a52d9f8807632630
Glitch (bustling-familiar-ceres) <none>
2021-10-26 18:56:36 +0000

    🎾🗺 Checkpoint ./src/hooks/volatile-state.jsx:35575323/298 ./src/hooks/game.jsx:35575323/70

--- src/hooks/game.jsx
+++ src/hooks/game.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 
+import useVolatileState from './volatile-state';
+
 export default function useGame() {
   const [state, setState] = useState(0);
-  const [users, setUsers] = useState({});
+  const [users, setUsers] = useVolatileState({});
   
   return {state, setState, users, setUsers}
 }
--- src/hooks/volatile-state.jsx
+++ src/hooks/volatile-state.jsx
@@ -1,6 +1,15 @@
 import { useState } from 'react';
 
-export default function useVolatileState() {
-  //A state that refreshes EVERY time a change is made.
+const useForceUpdate = () => {
+  const [index, updateIndex] = useState(0);
   
+  return () => updateIndex(i=>i+1);
+}
+
+export default function useVolatileState(def) {
+  //A state that refreshes EVERY time a change is made.
+  const [state, setState] = useState(def);
+  const forceUpdate = useForceUpdate()
+  
+  return (v) => {setState(v); forceUpdate()};
 }
