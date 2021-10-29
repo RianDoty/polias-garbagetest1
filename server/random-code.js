0eb5ab18aa717484ece7981565804215b8a537a0
Glitch (polias) <none>
2021-10-29 20:34:06 +0000

    🚐🎣 Checkpoint ./server/room.js:35575323/32 ./server/random-code.js:35575323/268

---
+++ server/random-code.js
@@ -0,0 +1,4 @@
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+module.exports = function randomCode(length = 4) {
+  
+}
