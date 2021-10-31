3e6cb5913e3bc2ab7721d7e887013e8366c136c0
Glitch (polias) <none>
2021-10-31 20:01:48 +0000

    🐴⚡️ Checkpoint ./server/random-code.js:35575323/236

--- server/random-code.js
+++ server/random-code.js
@@ -1,4 +1,12 @@
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 module.exports = function randomCode(length = 4) {
+  const data = '';
   
+  for (let i=0; i<length; i++) {
+    const randomIndex = Math.floor(Math.random()*letters.length);
+    
+    data += letters[randomIndex];
+  }
+  
+  return data
 }
