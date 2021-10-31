600962f18a50f2927bfe1b27744efa003328afa7
Glitch (polias) <none>
2021-10-31 22:10:50 +0000

    👚😅 Checkpoint ./server/random-code.js:35575323/58 ./package.json:35575323/15728

--- server/random-code.js
+++ server/random-code.js
@@ -1,3 +1,5 @@
+const usedLetters = {};
+
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 module.exports = function randomCode(length = 4) {
   const data = '';
@@ -7,6 +9,8 @@ module.exports = function randomCode(length = 4) {
     
     data += letters[randomIndex];
   }
+  
+  //Register a code as used
   
   return data
 }
