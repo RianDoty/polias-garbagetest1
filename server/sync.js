355019dfe70777cd2242158b156ac8c651b0d6dd
Glitch (polias) <none>
2021-11-01 00:04:27 +0000

    🌶🙅 Checkpoint ./server/sync.js:35575323/1401

--- server/sync.js
+++ server/sync.js
@@ -2,60 +2,11 @@ class SyncHost {
   constructor(io, keyword, startingData = {}) {
     this.io = io;
     this.keyword = keyword;
-    this._data = startingData;
+    this.data = startingData
+  }
+  
+  create(key, value) {
     
-    this.initProxy()
-  }
-  
-  initProxy() {
-    this.handler = {
-      set: (obj, prop, value) => {
-        const lastValue = obj[prop];
-        
-        if (value === lastValue) return;
-        
-        if (value === null && lastValue !== null) {
-          this.delete(prop, value)
-        } else if (lastValue === null && value !== null) {
-          this.create(prop, value)
-        } else if (lastValue !== value) {
-          this.update(prop, value)
-        }
-        
-        obj[prop] = value;
-        return true
-      }
-    }
-    
-    this.data = new Proxy(this._data, this.handler);
-  }
-  
-  create(prop, value) {
-    const { io, keyword } = this;
-    
-    io.to(keyword).emit(`create ${keyword}`, prop, value)
-  }
-  
-  update(prop, value) {
-    const { io, keyword } = this;
-    
-    io.to(keyword).emit(`update ${keyword}`, prop, value);
-  }
-  
-  delete(prop, value) {
-    const { io, keyword } = this;
-    
-    io.to(keyword).emit(`delete ${keyword}`, prop, value);
-  }
-  
-  subscribe(socket) {
-    const { keyword } = this;
-    socket.join(keyword);
-  }
-  
-  unsubscribe(socket) {
-    const { keyword } = this;
-    socket.leave(keyword);
   }
 }
 
