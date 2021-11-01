944189445e049ae52b857a0f2d948901a30ac378
Glitch (polias) <none>
2021-11-01 00:16:07 +0000

    🍞🌓 Checkpoint ./server/sync.js:35575323/867

--- server/sync.js
+++ server/sync.js
@@ -3,10 +3,31 @@ class SyncHost {
     this.io = io;
     this.keyword = keyword;
     this.data = startingData
+    
+    this.io.on('connection', s=>{
+      s.on(`subscribe ${keyword}`, ack=>this.subscribe(s,ack))
+    })
   }
   
   create(key, value) {
     
+  }
+  
+  update(key, prop, value) {
+    
+  }
+  
+  delete(key) {
+    
+  }
+  
+  subscribe(socket, ack) {
+    ack(this.data);
+    socket.join(this.keyword)
+  }
+  
+  unsubscribe(socket) {
+    socket.leave(this.keyword);
   }
 }
 
