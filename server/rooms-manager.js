ebc87c96165cf5918722acdfd6c2b0c9cbc5f7e2
Glitch (polias) <none>
2021-11-01 19:20:07 +0000

    🗻👱 Checkpoint ./src/socket.test.js:35575323/1016 ./server/rooms-manager.js:35575323/11 ./server/test-module.js:35575323/27

--- server/rooms-manager.js
+++ server/rooms-manager.js
@@ -18,6 +18,4 @@ module.exports = io => {
       ack(code);
     });
   });
-  
-  io.on
 };
---
+++ src/socket.test.js
@@ -0,0 +1,40 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const Client = require("socket.io-client");
+
+describe("Polias", () => {
+  let io, serverSocket, clientSocket;
+
+  beforeAll((done) => {
+    const httpServer = createServer();
+    io = new Server(httpServer);
+    httpServer.listen(() => {
+      const port = httpServer.address().port;
+      clientSocket = new Client(`http://localhost:${port}`);
+      io.on("connection", (socket) => {
+        serverSocket = socket;
+      });
+      clientSocket.on("connect", done);
+    });
+  });
+
+  afterAll(() => {
+    io.close();
+    clientSocket.close();
+  });
+  
+  
+  //Tests
+  test('transfers data', done => {
+    clientSocket.on('hello', arg => {
+      expect(arg).toBe('world')
+      done()
+    })
+    
+    serverSocket.emit('hello', 'world')
+  })
+  
+  test('requires outside modules', ()=>{
+    expect(require('../server/test-module')).toBe('foo')
+  })
+});
