38b3f17f19337210fd5bbf6c8ffe06968a59f51c
Glitch (polias) <none>
2021-11-03 21:17:16 +0000

    🕹🍥 Checkpoint ./server/room.js:35575323/72

--- server/room.js
+++ server/room.js
@@ -4,7 +4,8 @@ class Room {
   constructor(io, code, host, roomListHost) {
     this.io = io;
     this.code = code;
-    this.sync = new SyncHost();
+    this.playersSync = new SyncHost();
+    this.stateSync = new SyncHost();
   }
   
   updateList() {
