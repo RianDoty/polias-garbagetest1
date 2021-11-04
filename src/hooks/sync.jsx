0d143f3d2b25c40424d2319086e18cd912663977
Glitch (polias) <none>
2021-11-04 17:08:40 +0000

    🚟👀 Checkpoint ./src/hooks/sync.jsx:35575323/1907

--- src/hooks/sync.jsx
+++ src/hooks/sync.jsx
@@ -1,25 +1,37 @@
-import { useState, useEffect } from 'react';
-import { useSocket, useSocketCallbacks } from './socket'
-import useForceUpdate from './force-update';
+import { useState, useEffect } from "react";
+import { useVolatileState } from "./volatile-state";
+import { useSocket, useSocketCallbacks } from "./socket";
+import useForceUpdate from "./force-update";
 
-
-const useSync = (keyword) => {
+const useSync = keyword => {
   const socket = useSocket();
-  const [store, setStore] = useState();
-  const forceUpdate = useForceUpdate();
-  
-  const updateStore = (v) => {setStore(v); forceUpdate()}
-  
-  useEffect(()=>{
-    socket.emit(`sync subscribe ${keyword}`, s=>setStore(s));
+  const [store, setStore] = useVolatileState();
+
+  useEffect(() => {
+    socket.emit(`sync subscribe ${keyword}`, s => setStore(s));
     return () => socket.emit(`sync unsubscribe ${keyword}`);
-  },[keyword])
-  
+  }, [keyword]);
+
   useSocketCallbacks({
-    [`sync create ${keyword}`]: (key, value)=>{},
-    [`sync update ${keyword}`]: (key, prop, value)=>{},
-    [`sync delete ${keyword}`]: (key)=>{},
-  })
-  
-  return store
-}
+    [`sync create ${keyword}`]: (key, value) => {
+      setStore(store => {
+        store[key] = value;
+        return store;
+      });
+    },
+    [`sync update ${keyword}`]: (key, prop, value) => {
+      setStore(store => {
+        store[key][prop] = value;
+        return store
+      })
+    },
+    [`sync delete ${keyword}`]: key => {
+      setStore(store => {
+        delete store[key];
+        return store;
+      })
+    }
+  });
+
+  return store;
+};
