addf094f14c1de4f7bf3691902982902561d9a48
Glitch (polias) <none>
2021-11-09 22:23:21 +0000

    ⏱😋 Checkpoint ./src/components/game/player-list.jsx:35575323/157

--- src/components/game/player-list.jsx
+++ src/components/game/player-list.jsx
@@ -55,7 +55,10 @@ const avatars = [
   'https://cdn.glitch.me/35125d36-1414-4625-886d-50b6771f7d06%2Fbaseavatar.png?v=1636495856765',
   
 ]
+const clamp = (num, min, max) => Math.max(Math.min(num, max), min)
 const Avatar = ({cardID = 0}) => {
+  cardID = clamp(cardID, 0, avatars.length);
+  
   
 }
 
