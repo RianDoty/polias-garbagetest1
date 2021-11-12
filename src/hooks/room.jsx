3ebdce8ebab0214f7e074549720d919b5747a918
Glitch (polias) <none>
2021-11-12 18:35:10 +0000

    🌇👽 Checkpoint ./src/hooks/room.jsx:35575323/370

--- src/hooks/room.jsx
+++ src/hooks/room.jsx
@@ -3,11 +3,22 @@ import { useEffect, useContext } from 'react';
 import useSync from './sync';
 import { useSocket, useSocketFetch } from './socket';
 
+import UserContext from '../contexts/user';
+
+function stripFunctions(obj) {
+  const clone = Object.assign({}, obj)
+  clone.forEach((i,v) => {
+    if (typeof(v) === 'function') delete clone[i];
+  })
+  return clone
+}
+
 export default function useRoom(code) {
   const socket = useSocket()
+  const user = useContext(UserContext);
   
   useEffect(()=>{
-    socket.emit('join room', code);
+    socket.emit('join room', code, stripFunctions(user));
     return ()=>socket.emit('leave room', code);
   }, [])
 }
