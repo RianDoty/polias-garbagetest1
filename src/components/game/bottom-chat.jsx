41b9d1256309f7eb707a1ab0754c0aaaf9e88a2d
Glitch (polias) <none>
2021-11-18 17:49:00 +0000

    🐜🏅 Checkpoint ./src/components/game/bottom-chat.jsx:35575323/738

---
+++ src/components/game/bottom-chat.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect, useContext } from "react";
+import useSync from "../../hooks/sync";
+
+const BottomChat = () => {
+  return (
+    <div className="bottom-chat">
+    
+    </div>
+  );
+};
+
+const MessageEntry = () => {
+  return (
+    <div className='message-entry'>
+    
+    </div>
+  )
+}
