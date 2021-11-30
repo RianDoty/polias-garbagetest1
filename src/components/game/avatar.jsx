91c39b26409e07c96a33dbba3d4351b9401c62e5
Glitch (polias) <none>
2021-11-30 17:08:51 +0000

    🏈👗 Checkpoint ./src/components/game/avatar.jsx:35575323/45 ./src/styles/game.css:35575323/243

--- src/components/game/avatar.jsx
+++ src/components/game/avatar.jsx
@@ -2,10 +2,10 @@ const avatars = {
   0: 'https://cdn.glitch.me/35125d36-1414-4625-886d-50b6771f7d06%2Fbaseavatar.png?v=1636495856765',
 }
 
-const Avatar = ({cardID = 0, size = '40px'}) => {
+const Avatar = ({cardID = 0}) => {
   const image = avatars[cardID] || avatars[0];
   
-  return <img src={image} className='avatar' style={{height: `${size}`}}/>;
+  return <img src={image} className='avatar'/>;
 }
 
 export default Avatar
