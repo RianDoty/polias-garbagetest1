749eef2d2ef2950032d1d7dd74f3e9cd60755d82
Glitch (polias) <none>
2021-11-30 20:40:45 +0000

    🙏🐉 Checkpoint ./server/chat-manager.js:35575323/62

--- server/chat-manager.js
+++ server/chat-manager.js
@@ -3,8 +3,12 @@ const ChatRoom = require('./chat-room')
 
 class ChatRoomManager {
   constructor(io) {
+    this.io = io;
     
+    const rooms = {};
   }
+  
+  makeRoom()
 }
 
 module.exports = ChatRoomManager
