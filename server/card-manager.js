32398714982aca039e525364c56c070eacd9bde6
Glitch (polias) <none>
2021-12-09 17:49:51 +0000

    🎁🐃 Checkpoint ./server/card-manager.js:35575323/522 ./src/components/game/avatars.json:35575323/1557

--- server/card-manager.js
+++ server/card-manager.js
@@ -1,9 +1,13 @@
 const avatars = require('../src/components/game/avatars')
 
-
+function randomFromTable(tbl) {
+  const values = Object.entries(tbl);
+  const randomIndex = Math.floor(Math.random() * values.length);
+  return values[randomIndex] //[key, value]
+}
 class CardManager {
   constructor() {
-    console.log(`default avatar: ${avatars.default}`);
+    
   }
 }
 
